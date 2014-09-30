92d18ff7aadac79864860b699675fa5eca2fe7fb
satoshi_yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2014-09-30 19:24:34 +0900

    add the install script of dotfiles.

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FILES=(".bash_profile" ".bashrc" ".vimrc" ".vim")
+
+for file in "${FILES[@]}"
+do
+  currdir="$(pwd dirname)"
+  ln -s ${currdir}/$file ${HOME}/$file
+  if [ "$?" -eq 0 ]; then
+    echo " installing $file"
+  fi
+done
+
+echo "done."
