fbc9a90b4d5940d78519ad3b494aaf65d209100a
satoshi_yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2022-02-03 09:02:16 +0900

    [nvim] Set the workspace_dir for LSP server

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -7,6 +7,8 @@ vim.opt.termguicolors = true
 vim.opt.completeopt = 'menu,menuone,noselect'
 vim.opt.updatetime = 100
 
+vim.env.WORKSPACE = vim.fn.stdpath('data')..'/workspace'
+
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
