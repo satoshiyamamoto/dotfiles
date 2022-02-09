5aac4259fc365f8654b9528469a92662bcf7bb97
satoshi_yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2022-02-09 20:21:16 +0900

    style(nvim): Refactored

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -72,46 +72,49 @@ end)
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local map = vim.api.nvim_set_keymap
 local opts = { noremap=true, silent=true }
-vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d',       '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d',       '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+map('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+map('n', '[d',       '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+map('n', ']d',       '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+map('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
-vim.api.nvim_set_keymap('n', '<C-p>',      '<cmd>Telescope find_files<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', opts)
+map('n', '<C-p>',      '<cmd>Telescope find_files<CR>', opts)
+map('n', '<leader>ff', '<cmd>Telescope find_files<CR>', opts)
+map('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', opts)
+map('n', '<leader>fb', '<cmd>Telescope buffers<CR>', opts)
+map('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', opts)
 
-vim.api.nvim_set_keymap('n', '<C-n>',     '<cmd>NvimTreeToggle<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>r', '<cmd>NvimTreeRefresh<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>n', '<cmd>NvimTreeFindFile<CR>', opts)
+map('n', '<C-n>',     '<cmd>NvimTreeToggle<CR>', opts)
+map('n', '<leader>r', '<cmd>NvimTreeRefresh<CR>', opts)
+map('n', '<leader>n', '<cmd>NvimTreeFindFile<CR>', opts)
 
-vim.api.nvim_set_keymap('n', '<leader>`', '<cmd>split | wincmd j | resize 12 | terminal<CR>', opts)
-vim.api.nvim_set_keymap('i', 'jj', '<Esc>', opts)
+map('n', '<leader>`', '<cmd>split | wincmd j | resize 12 | terminal<CR>', opts)
+map('i', 'jj', '<Esc>', opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
+  local bufopt = vim.api.nvim_buf_set_option
+  local bufmap = vim.api.nvim_buf_set_keymap
   -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  bufopt(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD',        '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd',        '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K',         '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi',        '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>',     '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D',  '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr',        '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f',  '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  bufmap(bufnr, 'n', 'gD',        '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  bufmap(bufnr, 'n', 'gd',        '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  bufmap(bufnr, 'n', 'K',         '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  bufmap(bufnr, 'n', 'gi',        '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  bufmap(bufnr, 'n', '<C-k>',     '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  bufmap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  bufmap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  bufmap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  bufmap(bufnr, 'n', '<space>D',  '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  bufmap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  bufmap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  bufmap(bufnr, 'n', 'gr',        '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  bufmap(bufnr, 'n', '<space>f',  '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
 require("nvim-lsp-installer").on_server_ready(function(server)
@@ -156,6 +159,7 @@ cmp.setup {
     { name = 'vsnip' }, -- For vsnip users.
   }, {
     { name = 'buffer' },
+    { name = 'path' },
   }),
   formatting = {
     format = require('lspkind').cmp_format(),
