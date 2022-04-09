8e2d039bb244d6f2d546935a4e49d3855df41362
satoshi_yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2022-04-09 16:44:13 +0900

    fix(nvim): Fixes plug-in version

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -61,10 +61,10 @@ require('packer').startup(function(use)
   use 'kyazdani42/nvim-tree.lua'
 
   -- theme
-  use 'projekt0n/github-nvim-theme'
+  use {'projekt0n/github-nvim-theme', commit = '425f9de'}
   use 'nvim-lualine/lualine.nvim'
   use 'airblade/vim-gitgutter'
-  use 'akinsho/bufferline.nvim'
+  use {'akinsho/bufferline.nvim', branch = 'main'}
 
   if packer_bootstrap then
     require('packer').sync()
