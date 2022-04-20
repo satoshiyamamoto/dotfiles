3f9cbaa18ca85ae5b8e73c698cd4fc3f4af69006
satoshi_yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2022-04-20 17:09:06 +0900

    feat(kitty): Added kitty.conf

--- install.mac.sh
+++ install.mac.sh
@@ -11,11 +11,12 @@ ln -si ${WORKDIR}/tigrc ${HOME}/.tigrc
 
 ## Set the XDG config
 source ${HOME}/.zshenv
-mkdir -p ${XDG_CONFIG_HOME}/{git,fd,nvim,zsh}
+mkdir -p ${XDG_CONFIG_HOME}/{git,fd,kitty,nvim,zsh}
 ln -si ${WORKDIR}/config/zsh/zshrc ${ZDOTDIR}/.zshrc
 ln -si ${WORKDIR}/config/zsh/zpreztorc ${ZDOTDIR}/.zpreztorc
 ln -si ${WORKDIR}/config/git/config.mac ${XDG_CONFIG_HOME}/git/config
 ln -si ${WORKDIR}/config/fd/ignore ${XDG_CONFIG_HOME}/fd/ignore
+ln -si ${WORKDIR}/config/kitty/kitty.conf ${XDG_CONFIG_HOME}/kitty/kitty.conf
 ln -si ${WORKDIR}/config/nvim/init.lua ${XDG_CONFIG_HOME}/nvim/init.lua
 
 ## Set the Vimrc
