e65b45c6311645410254580086a04b24105819dd
satoshi_yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2022-04-25 20:57:57 +0900

    fix(nvim): Enabled Tab completion in cmdline

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -174,6 +174,7 @@ cmp.setup {
 
 -- Use buffer source for `/`
 cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = 'buffer' }
   }
@@ -181,6 +182,7 @@ cmp.setup.cmdline('/', {
 
 -- Use cmdline & path source for ':'
 cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = 'path' }
   }, {
