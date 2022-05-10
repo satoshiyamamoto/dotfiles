f5610568f19249def50327e1e1389685ed7708f4
satoshi_yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2022-05-10 20:41:59 +0900

    style(nvim): Polished

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -81,6 +81,10 @@ end)
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
+map('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+map('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 map('n', '<leader><leader>w', '<cmd>HopWord<CR>', opts)
 map('n', '<leader><leader>f', '<cmd>HopChar1<CR>', opts)
 map('n', '<leader>ff', '<cmd>Telescope find_files<CR>', opts)
@@ -93,60 +97,54 @@ map('n', '<leader>r', '<cmd>NvimTreeRefresh<CR>', opts)
 map('n', '<leader>n', '<cmd>NvimTreeFindFile<CR>', opts)
 map('i', 'jj', '<Esc>', opts)
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  local bufopt = vim.api.nvim_buf_set_option
-  local bufmap = vim.api.nvim_buf_set_keymap
-  -- Enable completion triggered by <c-x><c-o>
-  bufopt(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  bufmap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  bufmap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  bufmap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  bufmap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  bufmap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  bufmap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  bufmap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  bufmap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  bufmap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  bufmap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  bufmap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  bufmap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  bufmap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-  bufmap(bufnr, 'n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  bufmap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  bufmap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  bufmap(bufnr, 'n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-end
+require('nvim-lsp-installer').setup {}
 
 local lspconfig = require('lspconfig')
 
-require('nvim-lsp-installer').on_server_ready(function(server)
-  local opts = {
-    on_attach = on_attach,
-  }
+local on_attach = function(client, bufnr)
+  local option = vim.api.nvim_buf_set_option
+  local keymap = vim.api.nvim_buf_set_keymap
+  -- Enable completion triggered by <c-x><c-o>
+  option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  if server.name == 'sumneko_lua' then
-    opts.settings = {
-      Lua = {
-        diagnostics = {
-          globals = { 'vim' }
-        }
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+
+lspconfig.sumneko_lua.setup {
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
       }
     }
+  }
+}
+lspconfig.jdtls.setup {
+  on_attach = on_attach,
+  root_dir = function(fname)
+    return lspconfig.util.root_pattern('.git', 'mvnw', 'gradlew')(fname)
   end
-
-  if server.name == 'jdtls' then
-    opts.root_dir = function(fname)
-      return lspconfig.util.root_pattern('.git', 'mvnw', 'gradlew')(fname)
-    end
-  end
-
-  server:setup(opts)
-end)
+}
+lspconfig.gopls.setup { on_attach = on_attach }
+lspconfig.pyright.setup { on_attach = on_attach }
+lspconfig.tsserver.setup { on_attach = on_attach }
+lspconfig.terraformls.setup { on_attach = on_attach }
+lspconfig.rust_analyzer.setup { on_attach = on_attach }
 
 -- nvim-cmp setup
 local cmp = require('cmp')
@@ -208,8 +206,7 @@ cmp.event:on('confirm_done',
 require('nvim-autopairs').setup {}
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = 'all',
-  ignore_install = { 'phpdoc' },
+  ensure_installed = { 'c', 'go', 'java', 'javascript', 'typescript', 'python', 'lua', 'rust', 'yaml' },
   highlight = {
     enable = true,
     disable = {},
