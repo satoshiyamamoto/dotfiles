ef24c6dde1a6f5bca56f1fc4199cb531ab6fbaf1
satoshi_yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2022-07-01 20:42:17 +0900

    chore(tmux): Move configuration directory to $XDG_CONFIG_HOME

--- install.mac.sh
+++ install.mac.sh
@@ -4,19 +4,19 @@ WORKDIR="$(cd $(dirname $0) && pwd)"
 cd "$WORKDIR"
 
 # User HOME directories
-ln -si ${WORKDIR}/tmux.conf ${HOME}/.tmux.conf
 ln -si ${WORKDIR}/editorconfig ${HOME}/.editorconfig
 ln -si ${WORKDIR}/sqliterc ${HOME}/.sqliterc
 
 ## Set the XDG config
 source ${HOME}/.zshenv
-mkdir -p ${XDG_CONFIG_HOME}/{git,bat,fd,kitty,nvim/ftplugin}
+mkdir -p ${XDG_CONFIG_HOME}/{git,bat,fd,kitty,nvim/ftplugin,tmux}
 ln -si ${WORKDIR}/config/git/config.mac ${XDG_CONFIG_HOME}/git/config
 ln -si ${WORKDIR}/config/bat/config ${XDG_CONFIG_HOME}/bat/config
 ln -si ${WORKDIR}/config/fd/ignore ${XDG_CONFIG_HOME}/fd/ignore
 ln -si ${WORKDIR}/config/kitty/kitty.conf ${XDG_CONFIG_HOME}/kitty/kitty.conf
 ln -si ${WORKDIR}/config/nvim/init.lua ${XDG_CONFIG_HOME}/nvim/init.lua
 ln -si ${WORKDIR}/config/nvim/ftplugin/java.lua ${XDG_CONFIG_HOME}/nvim/ftplugin/java.lua
+ln -si ${WORKDIR}/config/tmux/tmux.conf ${XDG_CONFIG_HOME}/tmux/tmux.conf
 
 ## Set the Zsh config
 cat << 'EOL' >> ${XDG_CONFIG_HOME}/zsh/.zshrc
