6afbf11471bdcb6244b9e796d6957a689474daa3
satoshi_yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2022-07-25 22:08:23 +0900

    feat(nvim): Added configuration file for sqls

--- install.mac.sh
+++ install.mac.sh
@@ -9,13 +9,14 @@ ln -si ${WORKDIR}/sqliterc ${HOME}/.sqliterc
 
 ## Set the XDG config
 source ${HOME}/.zshenv
-mkdir -p ${XDG_CONFIG_HOME}/{git,bat,fd,kitty,nvim/ftplugin,tmux}
+mkdir -p ${XDG_CONFIG_HOME}/{git,bat,fd,kitty,nvim/ftplugin,sqls,tmux}
 ln -si ${WORKDIR}/config/git/config.mac ${XDG_CONFIG_HOME}/git/config
 ln -si ${WORKDIR}/config/bat/config ${XDG_CONFIG_HOME}/bat/config
 ln -si ${WORKDIR}/config/fd/ignore ${XDG_CONFIG_HOME}/fd/ignore
 ln -si ${WORKDIR}/config/kitty/kitty.conf ${XDG_CONFIG_HOME}/kitty/kitty.conf
 ln -si ${WORKDIR}/config/nvim/init.lua ${XDG_CONFIG_HOME}/nvim/init.lua
 ln -si ${WORKDIR}/config/nvim/ftplugin/java.lua ${XDG_CONFIG_HOME}/nvim/ftplugin/java.lua
+ln -si ${WORKDIR}/config/sqls/config.yml ${XDG_CONFIG_HOME}/sqls/config.yml
 ln -si ${WORKDIR}/config/tmux/tmux.conf ${XDG_CONFIG_HOME}/tmux/tmux.conf
 
 ## Set the Zsh config
