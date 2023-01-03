62ed402404068b272bc5758e846fe5469d733f27
satoshi_yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2023-01-03 09:15:26 +0900

    chore(nvim): Added keymap for folke/trouble

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -614,6 +614,13 @@ require("packer").startup(function(use)
         local hl = "DiagnosticSign" .. type
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
       end
+
+      vim.keymap.set("n", "<leader>xx", "<Cmd>TroubleToggle<CR>", {})
+      vim.keymap.set("n", "<leader>xw", "<Cmd>TroubleToggle workspace_diagnostics<CR>", {})
+      vim.keymap.set("n", "<leader>xd", "<Cmd>TroubleToggle document_diagnostics<CR>", {})
+      vim.keymap.set("n", "<leader>xl", "<Cmd>TroubleToggle loclist<CR>", {})
+      vim.keymap.set("n", "<leader>xq", "<Cmd>TroubleToggle quickfix<CR>", {})
+      vim.keymap.set("n", "gR", "<Cmd>TroubleToggle lsp_references<CR>", {})
     end,
     requires = {
       { "kyazdani42/nvim-web-devicons" },
