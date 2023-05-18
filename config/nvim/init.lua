d7690727a4b662908f361bc0a6f555fb74579351
satoshi_yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2023-05-18 18:58:19 +0900

    feat(nvim): Added github/copilot.vim plug-in

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -212,6 +212,7 @@ require("packer").startup(function(use)
         }, {
           { name = "buffer" },
           { name = "path" },
+          { name = "copilot" },
         }),
         formatting = {
           format = require("lspkind").cmp_format({
@@ -260,12 +261,14 @@ require("packer").startup(function(use)
       { "hrsh7th/cmp-buffer" },
       { "hrsh7th/cmp-path" },
       { "hrsh7th/cmp-cmdline" },
+      { "hrsh7th/cmp-copilot" },
       { "hrsh7th/cmp-vsnip" },
       { "hrsh7th/vim-vsnip" },
       { "hrsh7th/vim-vsnip-integ" },
     },
   })
 
+  use({ "github/copilot.vim" })
   use({ "onsails/lspkind-nvim" })
   use({ "golang/vscode-go" })
 
@@ -567,10 +570,10 @@ require("packer").startup(function(use)
         },
         extensions = {
           fzf = {
-            fuzzy = true,                   -- false will only do exact matching
+            fuzzy = true, -- false will only do exact matching
             override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true,    -- override the file sorter
-            case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
           },
           ["ui-select"] = {
             require("telescope.themes").get_dropdown(),
@@ -646,13 +649,13 @@ require("packer").startup(function(use)
       })
       vim.keymap.set("n", "<Leader>lg", function()
         require("toggleterm.terminal").Terminal
-            :new({
-              cmd = "lazygit",
-              direction = "float",
-              hidden = true,
-              count = 0,
-            })
-            :toggle()
+          :new({
+            cmd = "lazygit",
+            direction = "float",
+            hidden = true,
+            count = 0,
+          })
+          :toggle()
       end)
       vim.cmd([[
       autocmd TermOpen * startinsert
@@ -688,8 +691,7 @@ require("packer").startup(function(use)
           functions = {},
         },
         sidebars = { "qf", "vista_kind", "terminal", "packer" },
-        on_colors = function()
-        end,
+        on_colors = function() end,
       })
       -- vim.cmd([[colorscheme tokyonight]])
     end,
