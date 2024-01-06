58f81ac547f687ead2a6008ad3dc65cca8cdf1eb
satoshiyamamoto <satoshiyamamoto@me.com>
2024-01-06 17:38:09 +0900

    chore(nvim): Migrate null-ls to lsp-lint

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -402,6 +402,25 @@ local plugins = {
   },
 
   {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("lint").linters_by_ft = {
+        go = { "staticcheck" },
+        javascript = { "eslint_d" },
+        python = { "flake8", "mypy" },
+        sql = { "sqlfluff" },
+      }
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end,
+  },
+
+  {
     "williamboman/mason.nvim",
     build = ":MasonUpdate",
     cmd = {
