4826c0f0206db58ac557a7869ebbabeac0b80054
Satoshi Yamamoto <satoshiyamamoto@me.com>
2024-11-26 07:27:01 +0900

    chore(nvim): improve appearance of lualine

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1041,6 +1041,9 @@ local plugins = {
       require("lualine").setup({
         options = {
           theme = "auto",
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+          globalstatus = true,
         },
         sections = {
           lualine_x = {
