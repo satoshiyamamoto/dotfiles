8a307cae4dd98f910bfbc52b04f02aa14ded5eba
Satoshi Yamamoto <satoshiyamamoto@me.com>
2024-12-08 13:43:18 +0900

    chore(nvim): optimize basic options

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,20 +3,19 @@
 -- # Basics: {{{
 
 vim.g.mapleader = " "
-vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.laststatus = 3
 vim.opt.fillchars:append({ diff = " " })
 vim.opt.termguicolors = true
-vim.opt.confirm = true
 vim.opt.completeopt = { "menuone", "noselect" }
-vim.opt.clipboard:append({ "unnamedplus" })
+vim.opt.clipboard = "unnamedplus"
 vim.opt.shortmess:append({ c = true, I = true })
 vim.opt.updatetime = 100
-vim.opt.timeout = true
 vim.opt.timeoutlen = 300
 vim.opt.grepprg = "rg --vimgrep --hidden"
 vim.opt.grepformat = "%f:%l:%c:%m"
