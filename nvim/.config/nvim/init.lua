9fb1715bb67c2045200a057ef79cf0d9068cc7ad
Satoshi Yamamoto <satoshiyamamoto@me.com>
2026-03-01 11:58:26 +0900

    feat(nvim): add tiny-inline-diagnostic.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -971,6 +971,16 @@ local plugins = {
   },
 
   {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    config = function()
+      require("tiny-inline-diagnostic").setup()
+      vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
+    end,
+  },
+
+  {
     "kevinhwang91/nvim-bqf",
     ft = "qf",
   },
