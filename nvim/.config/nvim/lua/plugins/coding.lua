bd240fdaa43e3f64a9a3dd094094f991953e0627
Satoshi Yamamoto <satoshiyamamoto@me.com>
2026-06-09 23:07:54 +0900

    chore(homebrew): remove deprecated taps and tools, add zmx trust config

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -116,17 +116,6 @@ return {
     event = { "BufReadPost", "BufNewFile" },
   },
 
-  -- IME
-  {
-    "keaising/im-select.nvim",
-    event = "InsertEnter",
-    opts = {
-      default_im_select = "com.apple.inputmethod.Kotoeri.RomajiTyping.Roman",
-      default_command = "im-select",
-      set_default_events = { "InsertLeave", "CmdlineLeave" },
-    },
-  },
-
   -- Miscellaneous
   {
     "vim-jp/vimdoc-ja",
